f43aac12b5116e1240f46e960c457dae1fce4d30
samiutm <sami.mohaisen@mail.utoronto.ca>
2025-07-03 15:50:46 -0400

    Individual Push

--- src/main/java/data_access/InMemoryUserDataAccessObject.java
+++ src/main/java/data_access/InMemoryUserDataAccessObject.java
@@ -38,16 +38,12 @@ public class InMemoryUserDataAccessObject implements SignupUserDataAccessInterfa
     public void setCurrentUser(String name) {
         this.currentUser = name;
     }
-
     @Override
-    public String getCurrentUser() {
-        return this.currentUser;
-    }
+    public String getCurrentUser() {return this.currentUser;}
 
     @Override
     public void changePassword(User user) {
         // Replace the old entry with the new password
         users.put(user.getName(), user);
     }
-
 }
